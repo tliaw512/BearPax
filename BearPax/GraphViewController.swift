2659649168ce6150f55dc020c3945ed275210b79
Tiffany Liaw <tiffanyliaw@airbears2-10-142-191-18.airbears2.1918.berkeley.edu>
2017-05-01 14:26:11 -0700

    Added tableview for BAC and enzyme tracking.

---
+++ BearPax/GraphViewController.swift
@@ -0,0 +1,26 @@
+//
+//  GraphViewController.swift
+//  BearPax
+//
+//  Created by Tiffany Liaw on 5/1/17.
+//  Copyright © 2017 Tiffany Liaw. All rights reserved.
+//
+
+import UIKit
+
+class GraphViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+}
