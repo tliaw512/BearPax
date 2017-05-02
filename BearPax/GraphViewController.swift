e97bae656ff70ac7e62e8a1906db93fcdbb635eb
Tiffany Liaw <tiffanyliaw@airbears2-10-142-191-18.airbears2.1918.berkeley.edu>
2017-05-02 11:35:55 -0700

    Line graph works. Need to adjust axis.

--- BearPax/GraphViewController.swift
+++ BearPax/GraphViewController.swift
@@ -13,9 +13,11 @@ class GraphViewController: UIViewController {
 
     @IBOutlet weak var dataLabel: UILabel!
     @IBOutlet weak var descriptionText: UITextView!
-    
+    @IBOutlet weak var lineChart: LineChartView!
     
     var person: PeopleData?
+    var hours: [Double]?
+    var values: [Double]?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,9 +25,38 @@ class GraphViewController: UIViewController {
         if let person = person {
             dataLabel.text = "Current Levels: " + String(person.Hour5)
             descriptionText.text = person.Description
+            hours = [1, 2, 3, 4, 5]
+            values = [person.Hour1, person.Hour2, person.Hour3, person.Hour4, person.Hour5]
+            setChart(dataPoints: hours!, values: values!)
         }
 
     }
+    
+    func setChart(dataPoints: [Double], values: [Double]) {
+        var dataEntries: [ChartDataEntry] = []
+        
+        for i in 0..<dataPoints.count {
+            let dataEntry = ChartDataEntry(x: values[i], y: Double(i))
+            dataEntries.append(dataEntry)
+        }
+        
+        let lineChartDataSet = LineChartDataSet(values: dataEntries, label: (person?.type)! + " levels")
+        lineChartDataSet.setColor(UIColor.red)
+        lineChartDataSet.setCircleColor(UIColor.red)
+        lineChartDataSet.lineWidth = 2.0
+        lineChartDataSet.circleRadius = 6.0
+        lineChartDataSet.fillAlpha = 65 / 255.0
+        lineChartDataSet.fillColor = UIColor.red
+        lineChartDataSet.highlightColor = UIColor.white
+        lineChartDataSet.drawCircleHoleEnabled = true
+        
+        var dataSets = [IChartDataSet]()
+        dataSets.append(lineChartDataSet)
+        let lineChartData = LineChartData(dataSets: dataSets)
+        lineChart.data = lineChartData
+
+    }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
